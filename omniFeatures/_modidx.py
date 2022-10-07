1ae93dba0021d92155fe5c165eefa1c19ad52bb2
tkharrat <tarak@realanalytics.co.uk>
2022-10-07 18:46:29 +0100

    attempt to build a fully vectorised picth-control

--- omniFeatures/_modidx.py
+++ omniFeatures/_modidx.py
@@ -38,4 +38,5 @@ d = { 'settings': { 'allowed_cell_metadata_keys': '',
                                                       'omniFeatures.Features.distance_angle.Dist_ang.angle': 'https://tkharrat.github.io/omniFeatures/Features.distance_angle.html#dist_ang.angle',
                                                       'omniFeatures.Features.distance_angle.Dist_ang.dist': 'https://tkharrat.github.io/omniFeatures/Features.distance_angle.html#dist_ang.dist'},
             'omniFeatures.Features.pitch_control': {},
-            'omniFeatures.Features.pressurelines': { 'omniFeatures.Features.pressurelines.PressureLines': 'https://tkharrat.github.io/omniFeatures/Features.pressurelines.html#pressurelines'}}}
+            'omniFeatures.Features.pressurelines': { 'omniFeatures.Features.pressurelines.PressureLines': 'https://tkharrat.github.io/omniFeatures/Features.pressurelines.html#pressurelines'},
+            'omniFeatures.pitch_control': {}}}
